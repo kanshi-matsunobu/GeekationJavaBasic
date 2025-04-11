ce645f90c13ecdf04b7c5b1132be1bfcd687404e
kanshi-matsunobu <kanshi0107@gmail.com>
2025-04-11 15:34:07 +0900

    変数の課題

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,15 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int num1 = 13;
+		double num2 = 3.141592;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println("私の好きな整数は" + num1 + "です");
+		System.out.println("私の好きな小数は" + num2 + "です");
+	}
+
+}
